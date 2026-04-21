0dcfb9f724f9eb279e7965ea814906700c13ebf1
pkledeuS <cristian.np99@gmail.com>
2026-04-21 00:54:46 -0400

    configurar variables de entorno para produccion

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 export async function registrarUsuario (email: string, password: string) {
     const response = await fetch(`${API_URL}/registro/`, {
